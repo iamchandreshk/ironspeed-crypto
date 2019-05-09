2b257bb0a03d08f09c0d98937a34a52d22f3538e
Chandresh Khambhayata <hi@iamchandreshk.me>
2019-05-09 10:17:51 +0530

    Add project files.

---
+++ IronSpeedCrypto/ApplicationSettings.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+namespace IronSpeedCrypto
+{
+    public class ApplicationSettings
+    {
+        #region Declaration of ApplicationSettings
+        private static ApplicationSettings _Current = new ApplicationSettings();
+        public static ApplicationSettings Current
+        {
+            get
+            {
+                return ApplicationSettings._Current;
+            }
+        }
+        #endregion
+
+        #region Declaration of URLEncryptionKey
+        private string _URLEncryptionKey;
+        public string URLEncryptionKey
+        {
+            get
+            {
+                this._URLEncryptionKey = this.GetAppSetting("ApplicationName");
+                return this._URLEncryptionKey;
+            }
+            set
+            {
+                this._URLEncryptionKey = value;
+            }
+        }
+        #endregion
+
+        #region Declaration of DefaultEncryptionKey
+        private byte[] _DefaultEncryptionKey;
+        public byte[] DefaultEncryptionKey
+        {
+            get
+            {
+                this.GetDefaultEncryptionKey();
+                return this._DefaultEncryptionKey;
+            }
+            set
+            {
+                this._DefaultEncryptionKey = value;
+            }
+        }
+        #endregion
+
+
+        private ApplicationSettings()
+        {
+            this.GetDefaultEncryptionKey();
+            this._URLEncryptionKey = this.GetAppSetting("{B3015A1E-B8D2-4606-B014-42D3082634E8}"); // URLEncryptionKey
+        }
+
+        public void GetDefaultEncryptionKey()
+        {
+            string appSetting = this.GetAppSetting("78, 90, 23, 7, 54, 109, 34, 231, 90, 66, 109, 185, 228, 143, 89, 77, 190, 89, 103, 148, 54, 4, 98, 67, 243, 162, 68, 201, 73, 59, 184, 52"); // DefaultEncryptionKey
+            if (appSetting == null || string.IsNullOrEmpty(appSetting))
+                return;
+            List<byte> byteList = new List<byte>();
+            string[] strArray = appSetting.Split(',');
+            int index = 0;
+            while (index < strArray.Length)
+            {
+                byte result;
+                if (byte.TryParse(strArray[index], out result))
+                    byteList.Add(result);
+                checked { ++index; }
+            }
+            this._DefaultEncryptionKey = byteList.ToArray();
+        }
+
+        public string GetAppSetting(string key)
+        {
+            return key;
+            //return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
---
+++ IronSpeedCrypto/Crypto.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IronSpeedCrypto
+{
+    public class Crypto
+    {
+        private byte[] bytIV;
+        private SymmetricAlgorithm _CryptoService;
+
+        public Crypto()
+        {
+            this.bytIV = ApplicationSettings.Current.DefaultEncryptionKey;
+            this._CryptoService = (SymmetricAlgorithm)new RijndaelManaged();
+        }
+
+        private byte[] GetLegalKey(string Key)
+        {
+            string s;
+            if (this._CryptoService.LegalKeySizes.Length > 0)
+            {
+                int maxSize = this._CryptoService.LegalKeySizes[0].MaxSize;
+                int num = checked((int)Math.Round(unchecked((double)maxSize / 8.0)));
+                if (checked(Key.Length * 8) > maxSize)
+                {
+                    s = Key.Substring(0, num);
+                }
+                else
+                {
+                    int minSize = this._CryptoService.LegalKeySizes[0].MinSize;
+                    while (checked(Key.Length * 8) > minSize)
+                        checked { minSize += this._CryptoService.LegalKeySizes[0].SkipSize; }
+                    s = Key.PadRight(num, "X"[0]);
+                }
+            }
+            else
+                s = Key;
+            if (this._CryptoService.LegalBlockSizes.Length > 0)
+            {
+                int blockSize = this._CryptoService.BlockSize;
+                int num = checked((int)Math.Round(unchecked((double)blockSize / 8.0)));
+                this.bytIV = (byte[])CopyArray(this.bytIV, new byte[checked(s.Length - 1 + 1)]);
+                if (checked(s.Length * 8) > blockSize)
+                    this.bytIV = (byte[])CopyArray(this.bytIV, new byte[checked(num - 1 + 1)]);
+            }
+            return Encoding.ASCII.GetBytes(s);
+        }
+
+        private byte[] CopyArray(byte[] source, byte[] destination)
+        {
+            for (int i = 0; i < destination.Length; i++)
+            {
+                destination[i] = source[i];
+            }
+            return destination;
+        }
+
+
+        public virtual string Encrypt(string Source)
+        {
+            return this.Encrypt(Source, this.GetCryptoKeyWithoutSessionVariable(), Encoding.ASCII);
+        }
+
+        public string Encrypt(string Source, string Key, Encoding Encoding)
+        {
+            if (Source == null || Source.Trim().Length == 0)
+                return Source;
+
+            byte[] buffer = Encoding.ASCII.GetBytes(Source);
+
+            MemoryStream memoryStream = new MemoryStream();
+            this._CryptoService.Key = this.GetLegalKey(Key);
+            this._CryptoService.IV = this.bytIV;
+            ICryptoTransform encryptor = this._CryptoService.CreateEncryptor();
+            CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write);
+            cryptoStream.Write(buffer, 0, buffer.Length);
+            cryptoStream.FlushFinalBlock();
+            cryptoStream.Close();
+            byte[] array = memoryStream.ToArray();
+            memoryStream.Close();
+            return Convert.ToBase64String(array);
+        }
+
+        public virtual string Decrypt(string Source)
+        {
+            return this.Decrypt(Source, this.GetCryptoKeyWithoutSessionVariable(), Encoding.ASCII);
+        }
+
+        public virtual string Decrypt(string Source, string Key, Encoding Encoding)
+        {
+            if (Source == null || Source.Trim().Length == 0)
+                return Source;
+            try
+            {
+                byte[] buffer;
+                MemoryStream memoryStream1;
+                buffer = Convert.FromBase64String(Source);
+                memoryStream1 = new MemoryStream(buffer);
+                byte[] legalKey = this.GetLegalKey(Key);
+                this._CryptoService.Key = legalKey;
+                this._CryptoService.IV = this.bytIV;
+                ICryptoTransform decryptor1 = this._CryptoService.CreateDecryptor();
+                CryptoStream cryptoStream1 = new CryptoStream((Stream)memoryStream1, decryptor1, CryptoStreamMode.Read);
+                string str = "";
+                StreamReader streamReader = string.Compare(Encoding.EncodingName, Encoding.ASCII.EncodingName, false) != 0 ? (string.Compare(Encoding.EncodingName, Encoding.Unicode.EncodingName, false) != 0 ? new StreamReader((Stream)cryptoStream1, Encoding.GetEncoding(CultureInfo.CurrentUICulture.TextInfo.ANSICodePage)) : new StreamReader((Stream)cryptoStream1, Encoding.GetEncoding(CultureInfo.CurrentUICulture.TextInfo.ANSICodePage))) : new StreamReader((Stream)cryptoStream1, Encoding.GetEncoding(CultureInfo.CurrentUICulture.TextInfo.ANSICodePage));
+                str = streamReader.ReadToEnd();
+                streamReader.Close();
+                memoryStream1.Close();
+                cryptoStream1.Close();
+                return str;
+            }
+            catch { }
+            return Source;
+        }
+
+
+        private string GetCryptoKeyWithoutSessionVariable()
+        {
+            return "ffx4yp{B3015A1E-7845-1245-B014-42D3082634E8}xehl45";
+        }
+
+        public enum Providers
+        {
+            DES,
+            RC2,
+            Rijndael,
+            TripelDES,
+        }
+    }
+}
